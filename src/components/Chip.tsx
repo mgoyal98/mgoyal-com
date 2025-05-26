0d707c6e3d74ddf1fe7152a99b1a0c80d6513c48
Madhur Goyal <madhur.goyal2@gmail.com>
2025-05-26 22:18:52 +0530

    FIX: Chip Text Color

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,6 +1,6 @@
 export default function Chip({ text }: { text: string }) {
   return (
-    <div className='rounded-full bg-primary/10 text-primary border border-primary/50 px-2.5 py-0.5 text-xs md:text-sm'>
+    <div className='rounded-full bg-primary/10 text-foreground border border-primary/50 px-2.5 py-0.5 text-xs md:text-sm'>
       {text}
     </div>
   );
